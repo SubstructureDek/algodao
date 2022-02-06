1dcc3eb15b8cb209a15241c8256bc630f0746b38
dek <dek@substructure.one>
2022-02-06 10:28:14 -0500

    Update governance test for modified interface

--- tests/test_governance.py
+++ tests/test_governance.py
@@ -10,14 +10,14 @@ from algodao.voting import Proposal, ElectionToken
 def test_approvalgate():
     algod = algodao.helpers.createclient()
     creatorprivkey, creatoraddr = tests.helpers.create_funded(algod)
-    committee = Committee("Trusted", 5)
-    committee_id = committee.deploycontract(algod, creatorprivkey)
-    committee_addr = algosdk.logic.get_application_address(committee_id)
+    committee = Committee.CreateCommittee("Trusted", 5)
+    deployedcommittee = Committee.deploy(algod, committee, creatorprivkey)
+    committee_addr = algosdk.logic.get_application_address(deployedcommittee.appid)
     tests.helpers.fund_account(algod, committee_addr)
-    committee.call_inittoken(algod, creatorprivkey, creatoraddr)
-    algodao.helpers.optinasset(algod, creatoraddr, creatorprivkey, committee.assetid)
-    committee.call_setmembers(algod, creatorprivkey, creatoraddr, [creatoraddr])
-    creategate = PreapprovalGate.CreateGate(committee_id, 1)
+    deployedcommittee.call_inittoken(algod, creatorprivkey, creatoraddr)
+    algodao.helpers.optinasset(algod, creatoraddr, creatorprivkey, deployedcommittee.assetid)
+    deployedcommittee.call_setmembers(algod, creatorprivkey, creatoraddr, [creatoraddr])
+    creategate = PreapprovalGate.CreateGate(deployedcommittee.appid, 1)
     gate = PreapprovalGate.deploy(algod, creategate, creatorprivkey)
     algodao.helpers.optinapp(algod, creatorprivkey, creatoraddr, gate.appid)
     gate_addr = algosdk.logic.get_application_address(gate.appid)
@@ -32,7 +32,7 @@ def test_approvalgate():
         'http://localhost/abcd'
     )
     lastround = algod.status()['last-round']
-    proposal = Proposal(
+    proposal = Proposal.CreateProposal(
         "Test Proposal",
         ElectionToken(10),
         lastround,
@@ -41,9 +41,9 @@ def test_approvalgate():
         lastround+1000,
         2
     )
-    propappid = proposal.deploycontract(algod, creatorprivkey)
-    tests.helpers.fund_account(algod, algosdk.logic.get_application_address(propappid))
-    proposal.optintoken(algod, creatoraddr, creatorprivkey, gate._trust_asset_id)
-    gate.call_assessproposal(algod, creatoraddr, creatorprivkey, proposal._appid)
-    gate.call_vote(algod, creatoraddr, creatorprivkey, proposal._appid, 1)
+    deployedproposal = Proposal.deploy(algod, proposal, creatorprivkey)
+    tests.helpers.fund_account(algod, algosdk.logic.get_application_address(deployedproposal.appid))
+    deployedproposal.call_optintoken(algod, creatoraddr, creatorprivkey, gate._trust_asset_id)
+    gate.call_assessproposal(algod, creatoraddr, creatorprivkey, deployedproposal.appid)
+    gate.call_vote(algod, creatoraddr, creatorprivkey, deployedproposal.appid, 1)
 
